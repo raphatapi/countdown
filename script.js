1920cd6b4feeaeb7fa24ba70c0b9d1abbc1d4a2a
raphatapi <raphaeltapioca10@gmail.com>
2018-12-01 01:12:56 -0300

    refactored js code and added google font

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ $(document).ready(function() {
     var timer = $("<p></p>");
     timer.text(days + " days " + hours + " hours ").append("<br />").append(minutes + " minutes " + seconds + " seconds");
     $("#main").html(title).append(timer);
+    // clearInterval(x);
 
         // If the count down is finished, write some text 
         if (distance < 0) {
@@ -31,4 +32,10 @@ $(document).ready(function() {
             $("#main").css("backgroundImage", "url('./family.gif')");
         }
     }, 1000);
+
+    var calendar = $('<div>');
+    calendar.addClass("calendar");
+    calendar.append('<iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA&amp;height=400&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=raphaeltapioca10%40gmail.com&amp;color=%232952A3&amp;ctz=America%2FDenver" style="border-width:0" width="100%" height="auto" frameborder="0" scrolling="no"></iframe>');
+    $("#calendar").html(calendar);
+
 });
