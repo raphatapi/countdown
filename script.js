05d18854877b9c5e703ae6673febe5a8a8122a37
raphatapi <raphaeltapioca10@gmail.com>
2021-06-04 14:52:42 -0600

    new countdown

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     au.play();
     
     var title = $("<h1>");
-    title.text("My Birthday Starts in...");
+    title.text("Lockdengo strats in...");
     var timer = $("<div>");
     timer.addClass("clock");
     $("#main").html(title).append(timer);
@@ -13,7 +13,7 @@ $(document).ready(function() {
     // Grab the current date
     var currentDate = new Date().getTime();
     // Set some date in the future. In this case, it's always Jan 1
-    var futureDate = new Date("Feb 08, 2020 08:30:00").getTime();
+    var futureDate = new Date("Jul 23, 2021 08:30:00").getTime();
     // Calculate the difference in seconds between the future and current date
     var diff = futureDate / 1000 - currentDate / 1000;
     var clock = new FlipClock($('.clock'),diff, {
@@ -33,8 +33,8 @@ $(document).ready(function() {
     }); 
     
 
-    var calendar = $('<iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA&amp;height=400&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=raphaeltapioca10%40gmail.com&amp;color=%232952A3&amp;ctz=America%2FDenver" style="border-style:none" width="25%" height="104%" frameborder="0" scrolling="no" float="right"></iframe>');
-    calendar.addClass("calendar");
-    calendar.insertAfter("#main");
+    // var calendar = $('<iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA&amp;height=400&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=raphaeltapioca10%40gmail.com&amp;color=%232952A3&amp;ctz=America%2FDenver" style="border-style:none" width="25%" height="104%" frameborder="0" scrolling="no" float="right"></iframe>');
+    // calendar.addClass("calendar");
+    // calendar.insertAfter("#main");
 
 });
